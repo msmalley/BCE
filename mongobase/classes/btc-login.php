54b1898c4f80c1fc6c1bce53ca0d847ec9de9b0f
Mark Smalley <mark@smalley.my>
2013-12-04 07:29:23 +0800

    now with timed cookies

--- mongobase/classes/btc-login.php
+++ mongobase/classes/btc-login.php
@@ -20,13 +20,64 @@ class mongobase_btc_login extends mongobase_mb
 		$config = $this->ini();
 		$cookie_name = $config['app']['cookie_name'];
 		$btc_per_day = $config['app']['btc_per_day'];
+		$txid_salt = $config['salts']['txid'];
 		$uid_life = $config['app']['uid_life'];
 		$initial_cookie_life = 86400 * $uid_life;
 
 		$logged_in = false;
 		$uid_balance = $this::$btc->query(array('function'=>'getbalance', 'options'=>$cookie_name.'_'.$uid));
-		if($uid_balance > 0) $logged_in = true;
-		
+
+		if(is_array($_COOKIE))
+		{
+			$uid = false;
+			foreach($_COOKIE as $key => $value)
+			{
+				$key_array = explode('_', $key);
+				if(count($key_array) == 2 && $key_array[0] == $cookie_name)
+				{
+					$temp_uid = $key_array[1];
+					if(isset($_COOKIE[$cookie_name.'_'.$temp_uid]))
+					{
+						$txid = $_COOKIE[$cookie_name.'_'.$temp_uid];
+						$transactions = $this::$btc->query(array('function'=>'listtransactions', 'options'=>$cookie_name.'_'.$temp_uid));
+						if(is_array($transactions))
+						{
+							foreach($transactions as $transaction)
+							{
+								if(isset($transaction['txid']))
+								{
+									$hashed_id = hash('sha256', $txid_salt.$transaction['txid']);
+									if($hashed_id == $txid)
+									{
+										$logged_in = true;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		elseif($uid_balance > 0)
+		{
+			$recent_transactions = $this::$btc->query(array('function'=>'listtransactions', 'options'=>$cookie_name.'_'.$uid));
+			if(is_array($recent_transactions) && isset($recent_transactions[0]['txid']) && isset($recent_transactions[0]['amount']) && isset($recent_transactions[0]['category']) && $recent_transactions[0]['category'] == 'receive')
+			{
+				$txid = $recent_transactions[0]['txid'];
+				$amount = $recent_transactions[0]['amount'];
+
+				if($amount > 0)
+				{
+					$logged_in = true;
+					
+					$number_of_days_bought = $amount / $btc_per_day;
+					$new_cookie_life = 86400 * $number_of_days_bought;
+
+					setcookie($cookie_name.'_'.$uid, hash('sha256',$txid_salt.$txid), time() + $new_cookie_life);
+					setcookie($cookie_name, false, time() - 1);
+				}
+			}
+		}
 		return $logged_in;
 	}
 
@@ -36,10 +87,43 @@ class mongobase_btc_login extends mongobase_mb
 		$cookie_name = $config['app']['cookie_name'];
 		$btc_per_day = $config['app']['btc_per_day'];
 		$uid_life = $config['app']['uid_life'];
+		$txid_salt = $config['salts']['txid'];
 		$initial_cookie_life = 86400 * $uid_life;
 		
 		// Check if BTCUID Cookie Exists
-		if(isset($_COOKIE[$cookie_name]))
+		if(is_array($_COOKIE))
+		{
+			$uid = false;
+			foreach($_COOKIE as $key => $value)
+			{
+				$key_array = explode('_', $key);
+				if(count($key_array) == 2 && $key_array[0] == $cookie_name)
+				{
+					$temp_uid = $key_array[1];
+					if(isset($_COOKIE[$cookie_name.'_'.$temp_uid]))
+					{
+						$txid = $_COOKIE[$cookie_name.'_'.$temp_uid];
+						$transactions = $this::$btc->query(array('function'=>'listtransactions', 'options'=>$cookie_name.'_'.$temp_uid));
+						if(is_array($transactions))
+						{
+							foreach($transactions as $transaction)
+							{
+								if(isset($transaction['txid']))
+								{
+									$hashed_id = hash('sha256', $txid_salt.$transaction['txid']);
+									if($hashed_id == $txid)
+									{
+										$uid = $temp_uid;
+										$address = $this::$btc->query(array('function'=>'getaddressesbyaccount', 'options'=>$cookie_name.'_'.$uid));
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		elseif(isset($_COOKIE[$cookie_name]))
 		{
 			$uid = $_COOKIE[$cookie_name];
 
@@ -90,7 +174,13 @@ class mongobase_btc_login extends mongobase_mb
 		$html.= '<div id="main-container" class="container">';
 			if($logged_in)
 			{
-				$html.= '<div class="alert alert-success"><strong>Success</strong>: ACCESS GRANTED</div>';
+				$html.= '
+							<div class="alert alert-success">
+								<strong>Success</strong>: ACCESS GRANTED<br /><br />
+								You may reset this demo by clearing your cookies or waiting for it to expire based upon the amount you contributed at '.$btc_per_day.' per day.
+							</div>
+
+						';
 			}
 			else
 			{
